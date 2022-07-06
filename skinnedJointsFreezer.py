5ae4a0eae6a58d8a36f1a9deeec3d39e2b94620d
YGaotian <101716740+YGaotian@users.noreply.github.com>
2022-07-06 23:11:44 +0800

    Bugs fixed, add a progress bar

--- skinnedJointsFreezer.py
+++ skinnedJointsFreezer.py
@@ -1,33 +1,33 @@
 # -*- coding:utf-8 -*-
 
-'''
-Use this tool to freeze the joints bound to a mesh, no need to unbind the skin first.
+"""
+Use this tool to freeze the joints bound in a mesh, no need to unbind the skin first.
 Usage    : Select one or more skinned meshes, then run this script, notice, DO NOT select any other object, otherwise,
            an error should occur, if the error occurs, you should try to click Undo button to check if the last action
            is performed by yourself, not by the code
-Author   ：Y.Gaotian
+Author   : Y.Gaotian
 E-mail   : ygaotian@outlook.com || 2975410710@qq.com
-Create   ：2022-07-04
+Create   : 2022-07-05
 License  : Code licensed under the MIT License
 Important: Unlawful use of my code is prohibited
 All rights reserved
-'''
+"""
 
 import maya.cmds as cmds
 import maya.api.OpenMaya as om
 import maya.api.OpenMayaAnim as oma
 
 
-def getSkinClusterMObj(geoDag):
-    '''
+def get_skin_cluster_mobj(geoDag):
+    """
     geoDag: The MDagPath of the selected geometry
-    '''
+    """
     # get the fullPathName list of the skinCluster
     skinClusterTextList = cmds.ls(cmds.listHistory(geoDag.fullPathName()), type="skinCluster")
     if len(skinClusterTextList) < 1:
         return "No Skin Cluster Is Found"
 
-        # return the MObject for that skinCluster node
+    # return the MObject for that skinCluster node
     clusterSelList = om.MSelectionList()
     clusterSelList.add(skinClusterTextList[0])
     clusterMObj = clusterSelList.getDependNode(0)
@@ -39,62 +39,122 @@ def rebinding():
     if actvSelList.length() < 1:
         return "Please Select At Least One Geometry"
 
-    selListIter = om.MItSelectionList(actvSelList)
-    while (not selListIter.isDone()):
-        actvSelDag = selListIter.getDagPath()
-        actvSelShape = actvSelDag.extendToShape()
-        actvSelObj = actvSelShape.node()
+    listOfJointsDagPathList = []  # a list contains the joint influences list of every geo in MSelectionList
+    listOfJointInfluencesIDList = []  # a list contains the joint influences IDs list of each geo in this ↑
+    listOfAllJoints = []  # a set of all the joints we want to freeze
+    listOfAllWeightsList = []  # a list contains allWeightsList created in every loop
 
-        # get skin cluster
-        skinClusterObj = getSkinClusterMObj(actvSelDag)
+    # progressBar
+    progressBar = cmds.progressWindow(title="Processing",
+                                      status="Recording Weights: Please wait",
+                                      isInterruptable=1)
 
-        # make MFnSkinCluster
-        mfnSkinCluster = oma.MFnSkinCluster(skinClusterObj)
+    try:
+        selListIter = om.MItSelectionList(actvSelList)
+        while not selListIter.isDone():
+            currentSelItemDag = selListIter.getDagPath()
+            currentSelItemShape = currentSelItemDag.extendToShape()
+            currentSelItemObj = currentSelItemShape.node()
 
-        # get all joints
-        jointsDagPathList = mfnSkinCluster.influenceObjects()
-        jointInfluencesIDList = om.MIntArray()
-        JID = 0
-        for i in jointsDagPathList:
-            jointInfluencesIDList.append(JID)
-            JID += 1
+            # get skin cluster
+            skinClusterObj = get_skin_cluster_mobj(currentSelItemDag)
 
-        # record weights
-        allWeightsList = []  # A 2-D list that contains the weight of all joints on every vertex
-        vtxOnMeshIter = om.MItMeshVertex(actvSelObj)
-        while (not vtxOnMeshIter.isDone()):
-            '''
-            getWights() returns a tuple that contains a list of weight of every joint, and, the number of joints
-            So allWeightsList should appends getWeights()[0]
-            '''
-            allWeightsList.append(mfnSkinCluster.getWeights(actvSelShape, vtxOnMeshIter.currentItem())[0])
-            # print mfnSkinCluster.getWeights(actvSelShape, vtxOnMeshIter.currentItem())[0]
-            vtxOnMeshIter.next()
+            # make MFnSkinCluster
+            mfnSkinCluster = oma.MFnSkinCluster(skinClusterObj)
 
-        # unbinding
-        cmds.skinCluster(actvSelDag.fullPathName(), e=1, ub=1)
+            # get all joints
+            jointsDagPathList = mfnSkinCluster.influenceObjects()
+            listOfJointsDagPathList.append(jointsDagPathList)
+            jointInfluencesIDList = om.MIntArray()
+            JID = 0
+            for i in jointsDagPathList:
+                # record all joints we need to freeze
+                if i not in listOfAllJoints:
+                    listOfAllJoints.append(i)
+                # record all joint influences' IDs into the list in this loop
+                jointInfluencesIDList.append(JID)
+                JID += 1
+            # gathering the jointInfluencesIDList into a list
+            listOfJointInfluencesIDList.append(jointInfluencesIDList)
+
+            # record weights
+            allWeightsList = []  # A 2-D list that contains the weight of all joints on every vertex
+            vtxOnMeshIter = om.MItMeshVertex(currentSelItemObj)
+            cmds.progressWindow(progressBar, edit=1, maxValue=vtxOnMeshIter.count())
+            while not vtxOnMeshIter.isDone():
+                """
+                getWights() returns a tuple that contains a list of weight of every joint, and, the number of joints
+                So allWeightsList should appends getWeights()[0]
+                """
+                allJointsWeightAtCurrentVtx = mfnSkinCluster.getWeights(currentSelItemShape,
+                                                                        vtxOnMeshIter.currentItem())[0]
+                allWeightsList.append(allJointsWeightAtCurrentVtx)
+
+                # progress++
+                cmds.progressWindow(progressBar, edit=1, step=1)
+                vtxOnMeshIter.next()
+
+            listOfAllWeightsList.append(allWeightsList)
+
+            # unbinding
+            cmds.skinCluster(currentSelItemDag.fullPathName(), e=1, ub=1)
+            selListIter.next()
+
+        # progress amount reset
+        cmds.progressWindow(progressBar, edit=1, progress=0, status="Freezing Rotation: Please wait",
+                            maxValue=len(listOfAllJoints))
 
         # freeze joints
-        for each in jointsDagPathList:
+        for each in listOfAllJoints:
             cmds.makeIdentity(each, apply=1, r=1)
+            # progress++
+            cmds.progressWindow(progressBar, edit=1, step=1)
 
-        # rebind the skin
-        cmds.skinCluster(actvSelDag.fullPathName(), jointsDagPathList)
+        # rebinding and restore weights
+        currentGeoIndex = 0
+        new_selListIter = om.MItSelectionList(actvSelList)
 
-        # restore weights
-        new_skinClusterObj = getSkinClusterMObj(actvSelDag)
-        new_mfnSkinCluster = oma.MFnSkinCluster(new_skinClusterObj)
-        new_vtxOnMeshIter = om.MItMeshVertex(actvSelObj)
-        while (not new_vtxOnMeshIter.isDone()):
-            currentVtx = new_vtxOnMeshIter.currentItem()
-            currentVtxID = new_vtxOnMeshIter.index()
-            new_mfnSkinCluster.setWeights(actvSelDag,
-                                          currentVtx,
-                                          jointInfluencesIDList,
-                                          allWeightsList[currentVtxID])
-            new_vtxOnMeshIter.next()
+        # progress amount reset
+        cmds.progressWindow(progressBar, edit=1, progress=0, status="Rebinding Skins: Please wait")
 
-        selListIter.next()
+        while not new_selListIter.isDone():
+            new_currentSelItemDag = new_selListIter.getDagPath()
+            new_currentSelItemObj = new_currentSelItemDag.extendToShape().node()
+
+            jointNamesForSkinningList = []
+            for i in listOfJointsDagPathList[currentGeoIndex]:
+                jointNamesForSkinningList.append(i.fullPathName())
+
+            # bind skin
+            cmds.skinCluster(new_currentSelItemDag.fullPathName(),
+                             jointNamesForSkinningList, tsb=1)
+
+            new_skinClusterObj = get_skin_cluster_mobj(new_currentSelItemDag)
+            new_mfnSkinCluster = oma.MFnSkinCluster(new_skinClusterObj)
+            new_vtxOnMeshIter = om.MItMeshVertex(new_currentSelItemObj)
+
+            # progressBar set maxValue
+            cmds.progressWindow(progressBar, edit=1, maxValue=new_vtxOnMeshIter.count())
+
+            # get jointInfluencesIDList and allWeightsList corresponding to the current loop
+            new_jointInfluencesIDList = listOfJointInfluencesIDList[currentGeoIndex]
+            new_allWeightsList = listOfAllWeightsList[currentGeoIndex]
+            while not new_vtxOnMeshIter.isDone():
+                currentVtx = new_vtxOnMeshIter.currentItem()
+                currentVtxID = new_vtxOnMeshIter.index()
+                new_mfnSkinCluster.setWeights(new_currentSelItemDag, currentVtx,
+                                              new_jointInfluencesIDList, new_allWeightsList[currentVtxID])
+
+                # progress++
+                cmds.progressWindow(progressBar, edit=1, step=1)
+                new_vtxOnMeshIter.next()
+
+            currentGeoIndex += 1
+            new_selListIter.next()
+    except:
+        cmds.progressWindow(progressBar, edit=1, endProgress=1)
+        raise
+    cmds.progressWindow(progressBar, edit=1, endProgress=1)
 
 
 rebinding()
